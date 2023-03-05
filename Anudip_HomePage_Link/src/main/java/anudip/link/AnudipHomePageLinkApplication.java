37b73c1c4e77f7a33a17a2495427cc35ae037bf1
PratikNanavare <pratiknanavare890@gmail.com>
2023-03-05 11:56:16 +0530

    09-03-23 1st Assignment

---
+++ Anudip_HomePage_Link/src/main/java/anudip/link/AnudipHomePageLinkApplication.java
@@ -0,0 +1,13 @@
+package anudip.link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnudipHomePageLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnudipHomePageLinkApplication.class, args);
+	}
+
+}
---
+++ Anudip_HomePage_Link/src/test/java/anudip/link/AnudipHomePageLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package anudip.link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnudipHomePageLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
