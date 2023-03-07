39c826294d35517fca2852fc1af697543a8d07ab
PratikNanavare <pratiknanavare890@gmail.com>
2023-03-07 20:13:02 +0530

    09-03-23 3rd Assignment

---
+++ Coffee_Production_House/src/main/java/cofee/production/CoffeeProductionHouseApplication.java
@@ -0,0 +1,13 @@
+package cofee.production;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeProductionHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeProductionHouseApplication.class, args);
+	}
+
+}
---
+++ Coffee_Production_House/src/test/java/cofee/production/CoffeeProductionHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package cofee.production;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeProductionHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
